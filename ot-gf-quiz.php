f5f69aa3292071d86d9a4a4686fc13322fbb8b3f
josh kennedy <josh@outthinkgroup.com>
2022-04-12 12:20:15 -0400

    init :

---
+++ ot-gf-quiz.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Outthink Quiz Calc
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ot-gf-quiz
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ot_Gf_Quiz
+ */
+
+// Your code starts here.
+
